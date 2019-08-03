4f25937a497c7ff642c23c15b5e8c8767f086e5e
mracar07 <umutcan@umutcanacar.me>
2019-08-03 14:34:18 +0000

    RR-ify
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

---
+++ rr_guacamoleb.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamoleb device
+$(call inherit-product, device/oneplus/guacamoleb/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := rr_guacamoleb
+PRODUCT_DEVICE := guacamoleb
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 7
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Build info
+BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:9/PKQ1.190110.001/1905051527:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus7 \
+    PRODUCT_NAME=OnePlus7 \
+    PRIVATE_BUILD_DESC="OnePlus7-user 9 PKQ1.190110.001 1905051527 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
